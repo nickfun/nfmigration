c8e79aeef937a4c1c0092127203055099ae82c89
Nick Fun <nick@nick.gs>
2014-05-06 16:49:08 -0700

    writing a command to generate a migration

---
+++ MigrateNewCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateNewCommand extends Command {
+
+    private $sDirPath = "./";
+
+    protected function configure() {
+        $this->setName("migrate:new")
+                ->setDescription("Create a new Migration")
+                ->addArgument("system", InputArgument::REQUIRED, "System the migration will run against")
+                ->addArgument("name", InputArgument::REQUIRED, "Name of the migration. No Spaces.");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $system = $input->getArgument("system");
+        $name = $input->getArgument("name");
+        if (strpos($name, " ") > 0 ) {
+            $output->writeln("<error>ERROR</error> name can not have spaces.");
+            $output->writeln("A migration was NOT created");
+            return;
+        }
+        $output->writeln("System:\t$system");
+        $output->writeln("Name:\t$name");
+        $output->writeln("OK!");
+    }
+
+}
--- application.php
+++ application.php
@@ -1,11 +1,14 @@
 <?php
 
 require "vendor/autoload.php";
+
 require "GreetCommand.php";
+require "MigrateNewCommand.php";
 
 use Symfony\Component\Console\Application;
 
 $app = new Application();
 $app->add(new GreetCommand());
+$app->add(new MigrateNewCommand());
 $app->run();
 echo "Hello";
