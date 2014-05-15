e3cee3fba50483add2806437164cab37e4756f48
Nick Fun <nick@nick.gs>
2014-05-15 16:39:04 -0700

    some notes, a stub RUN command

---
+++ MigrateRunCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateRunCommand extends Command {
+
+    private $sDirPath = "./migrations/";
+
+    protected function configure() {
+        $this->setName("migrate:run")
+			->setDescription("Run Migrations on a System")
+			->addArgument("system", InputArgument::REQUIRED, "System the migration will run against");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $system = $input->getArgument("system");
+		$dirName = $this->sDirPath . $system;
+
+		$output->writeln("Run Migrations <info>$system</info>");
+		$output->writeln("Migrations are in $dirName");
+		
+		if (!file_exists($dirName)) {
+			$output->writeln("<error>Can not open folder for $system</error>");
+			return;
+		}
+		$dir = dir($dirName);
+		while ($file = $dir->read()) {
+			$output->writeln($file);
+		}
+    }
+
+}
--- application.php
+++ application.php
@@ -4,11 +4,13 @@ require "vendor/autoload.php";
 
 require "GreetCommand.php";
 require "MigrateNewCommand.php";
+require "MigrateRunCommand.php";
 
 use Symfony\Component\Console\Application;
 
 $app = new Application();
 $app->add(new GreetCommand());
 $app->add(new MigrateNewCommand());
+$app->add(new MigrateRunCommand());
 $app->run();
 echo "Hello";
