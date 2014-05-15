02d950d8c69f5c833c827d0bb94a7196ab65172f
Nick Fun <nick@nick.gs>
2014-05-15 16:43:22 -0700

    Stub for an INSTALL command. I need to re-think the namespace of the commands. just drop it.

---
+++ MigrateInstallCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateInstallCommand extends Command {
+
+    private $sDirPath = "./migrations/";
+
+    protected function configure() {
+        $this->setName("migrate:install")
+			->setDescription("Install NFMigrate on all systems");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+		$output->writeln("This will <info>install</info> the NFMigrate tables onto all systems.");
+		$output->writeln("Is this correct?");
+		$systems = ['HEAD','BODY','SYS','WAREHOUSE'];
+		foreach ($systems as $name) {
+			$output->writeln("\t$name");
+		}
+		
+    }
+
+}
+
--- application.php
+++ application.php
@@ -5,6 +5,7 @@ require "vendor/autoload.php";
 require "GreetCommand.php";
 require "MigrateNewCommand.php";
 require "MigrateRunCommand.php";
+require "MigrateInstallCommand.php";
 
 use Symfony\Component\Console\Application;
 
@@ -12,5 +13,5 @@ $app = new Application();
 $app->add(new GreetCommand());
 $app->add(new MigrateNewCommand());
 $app->add(new MigrateRunCommand());
+$app->add(new MigrateInstallCommand());
 $app->run();
-echo "Hello";
