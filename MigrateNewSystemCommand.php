6ec08932c95edd79e33a57b72b26d800d073b4e5
Nick Fun <nick@nick.gs>
2014-05-22 16:28:13 -0700

    Another new stub: create a new system

---
+++ MigrateNewSystemCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+// Migrate Check Command
+// Runs a test connection on all the defined systems, lets the user know if they worked or not.
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateNewSystemCommand extends Command {
+
+	private $sDirPath = "./";
+
+	protected function configure() {
+		$this->setName("migrate:newsystem");
+		$this->setDescription("Check system connections");
+		$this->addArgument("name", InputArgument::REQUIRED, "Name of the system you are creating");
+	}
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+		$name = $input->getArgument("name");
+		$output->writeln("New System: <info>$name</info>");
+		$fileName = "/systems/$name.php";
+		$output->writeln("Filename: <info>$fileName</info>");
+    }
+
+}
--- application.php
+++ application.php
@@ -7,12 +7,14 @@ require "MigrateNewCommand.php";
 require "MigrateRunCommand.php";
 require "MigrateInstallCommand.php";
 require "MigrateCheckCommand.php";
+require "MigrateNewSystemCommand.php";
 
 use Symfony\Component\Console\Application;
 
 $app = new Application();
 //$app->add(new GreetCommand());
 $app->add(new MigrateNewCommand());
+$app->add(new MigrateNewSystemCommand());
 $app->add(new MigrateRunCommand());
 $app->add(new MigrateInstallCommand());
 $app->add(new MigrateCheckCommand());
