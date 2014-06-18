7331c86066922bc1d3dcfaae9bd4abc4a0869c27
Nick Fun <nick@nick.gs>
2014-06-17 18:24:28 -0700

    Update System tempalte to make it clear you must return an array of Connections

--- template-system.php
+++ template-system.php
@@ -6,9 +6,15 @@ class TEMPLATE_SYSTEM {
     private $user = "testuser";
     private $pass = "testpassword";
 
+	/**
+	 * Build and return an array of PDO connections
+	 * @return array
+	 */
     public function getConnections() {
         // return an array of PDO connections
         throw new \Exception("The system TEMPLATE_SYSTEM has not been set up");
+		$db = new PDO($this->dsn, $this->user, $this->pass);
+		return array($db);
     }
 
     public function check(PDO $conn) {
